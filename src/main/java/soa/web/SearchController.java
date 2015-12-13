28c82b899af43f950eedbca2d8568f632cc47033
oscarmeler <oscarmeler91@gmail.com>
2015-12-13 23:59:01 +0100

    query interface extended

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import java.util.Map;
+import java.util.HashMap;
 
 @Controller
 public class SearchController {
@@ -22,7 +23,10 @@ public class SearchController {
 
     @RequestMapping(value="/search")
     @ResponseBody
-    public Object search(@RequestParam("q") String q) {
+    public Object search(@RequestParam("q") String q, @RequestParam(value = "max", required = false) Integer max) {
+      Map<String, Object> headers = new HashMap<String, Object>();
+    	headers.put("CamelTwitterKeywords", q);
+    	if (max != null) headers.put("CamelTwitterCount", max);
         return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
     }
 }
